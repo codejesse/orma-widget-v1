7d2f4d9bd2e770af5c1c6658b7bee2e2d3b5539a
codejesse <jessebeke10@gmail.com>
2025-04-11 23:16:08 +0100

    feat: modified vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,23 +2,30 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
+import rollupPolyfillNode from 'rollup-plugin-polyfill-node';
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      process: 'process/browser',
+    },
+  },
   build: {
     lib: {
       entry: resolve(__dirname, 'src/embed.jsx'),
       name: 'OrmaFeedbackWidget',
-      fileName: (format) => `feedback-widget.${format}.js`
+      fileName: (format) => `feedback-widget.${format}.js`,
     },
     rollupOptions: {
       output: {
         globals: {
           react: 'React',
-          'react-dom': 'ReactDOM'
-        }
+          'react-dom': 'ReactDOM',
+        },
       },
-      external: ['react', 'react-dom']
-    }
-  }
+      external: ['react', 'react-dom'],
+      plugins: [rollupPolyfillNode()],
+    },
+  },
 });
