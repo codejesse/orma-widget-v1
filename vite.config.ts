ce369b0c33ffb8e7e191457b3eab745c4ebb00e5
codejesse <jessebeke10@gmail.com>
2025-04-11 23:25:09 +0100

    feat: modified vite.config.ts (4)

--- vite.config.ts
+++ vite.config.ts
@@ -1,35 +1,41 @@
 // vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
-import rollupNodePolyFill from 'rollup-plugin-node-polyfills';
-import inject from '@rollup/plugin-inject';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import rollupNodePolyFill from "rollup-plugin-node-polyfills";
+import inject from "@rollup/plugin-inject";
 
+// https://vitejs.dev/config/
 export default defineConfig({
+  define: {
+    "process.env": {}, // Define process.env to avoid undefined errors
+  },
   plugins: [react()],
   resolve: {
     alias: {
-      process: 'process/browser', // Alias for process
+      "@": path.resolve(__dirname, "./src"),
+      process: "process/browser", // Alias for process
     },
   },
   build: {
     lib: {
-      entry: resolve(__dirname, 'src/embed.jsx'),
-      name: 'OrmaFeedbackWidget',
-      fileName: (format) => `feedback-widget.${format}.js`,
+      entry: "./src/index.jsx",
+      name: "widget",
+      fileName: (format) => `widget.${format}.js`,
     },
+    target: "esnext",
     rollupOptions: {
       output: {
         globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
+          react: "React",
+          "react-dom": "ReactDOM",
         },
       },
-      external: ['react', 'react-dom'], // Ensure React is external
+      external: ["react", "react-dom"], // Ensure React is external
       plugins: [
         rollupNodePolyFill, // Polyfill Node.js modules
         inject({
-          process: 'process/browser', // Inject process polyfill
+          process: "process/browser", // Inject process polyfill
         }),
       ],
     },
