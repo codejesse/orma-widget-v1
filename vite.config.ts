9d165f0401692b04b23d80cd3eb1b717121977c2
codejesse <jessebeke10@gmail.com>
2025-05-20 09:05:24 +0100

    fix: added css injector for the tailwindcss styling issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 
 const isES = process.env.BUILD === "es";
 const external = isES ? ["react", "react-dom"] : [];
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), cssInjectedByJsPlugin()],
   define: {
     "process.env.NODE_ENV": JSON.stringify("production"),
     "process.env": {}, // prevent ReferenceError for any other process.env usage
